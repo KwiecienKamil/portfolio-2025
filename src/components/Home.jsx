bed82d86c00dca451e5fd27cfff0281b0d26a6f1
Kamil <kamilkwiecien9@gmail.com>
2025-09-22 22:44:11 +0200

    Style projects section

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,5 +1,4 @@
-import { motion, useScroll } from "framer-motion";
-import { useState, useEffect, useRef, Suspense } from "react";
+import {  useRef } from "react";
 
 import { useGSAP } from "@gsap/react";
 import gsap from "gsap";
